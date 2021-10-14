41e20a966463dd52aa2075a9db3bb5262454f767
George Crosby <george@cogitogroup.co.uk>
2021-10-14 08:44:37 +0100

    updating ss

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,2 @@
 #!/bin/zsh
-printf $ANYCONNECT_PASSWORD | openconnect $ANYCONNECT_SERVER --passwd-on-stdin --user=$ANYCONNECT_USER --timestamp --servercert=sha256:af46c30e4eb24b0d2c0d9d70b59cc8511ac9863cadce476c0704198009f4cb83
-
+printf $ANYCONNECT_PASSWORD | openconnect $ANYCONNECT_SERVER --passwd-on-stdin --user=$ANYCONNECT_USER --timestamp --servercert=sha256:af46c30e4eb24b0d2c0d9d70b59cc8511ac9863cadce476c0704198009f4cb83 
